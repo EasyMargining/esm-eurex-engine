d893c52fdbe2889f5791c2c1ad48a0e1f866b988
Gilles MARCHAL <g_marchal2000@yahoo.fr>
2016-05-17 12:40:56 +0200

    Update Jhipster 3.3.0

--- src/main/java/com/easymargining/EsmeurexengineApp.java
+++ src/main/java/com/easymargining/EsmeurexengineApp.java
@@ -2,21 +2,18 @@ package com.easymargining;
 
 import com.easymargining.config.Constants;
 import com.easymargining.config.JHipsterProperties;
+
 import com.easymargining.domain.EurexMarketDataEnvironment;
-import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -26,9 +23,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @ComponentScan
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class })
 @EnableConfigurationProperties({ JHipsterProperties.class })
 @EnableEurekaClient
 public class EsmeurexengineApp {
@@ -48,21 +47,20 @@ public class EsmeurexengineApp {
     @PostConstruct
     public void initApplication() {
         if (env.getActiveProfiles().length == 0) {
-            log.warn("No Spring profile configured, running with default configuration");
+            log.warn("No Spring profile configured, running with default profile: {}", Constants.SPRING_PROFILE_DEVELOPMENT);
         } else {
             log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
             Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
             if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
-                log.error("You have misconfigured your application! " +
-                    "It should not run with both the 'dev' and 'prod' profiles at the same time.");
+                log.error("You have misconfigured your application! It should not run " +
+                    "with both the 'dev' and 'prod' profiles at the same time.");
             }
             if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
-                log.error("You have misconfigured your application! " +
-                    "It should not run with both the 'dev' and 'cloud' profiles at the same time.");
+                log.error("You have misconfigured your application! It should not" +
+                    "run with both the 'dev' and 'cloud' profiles at the same time.");
             }
         }
     }
-
 
     /**
      * Main method, used to run the application.
@@ -71,13 +69,10 @@ public class EsmeurexengineApp {
      *             -vd 2015-11-23
      *             -m D:\gmarchal\eurex-data\marketdata
      * @throws UnknownHostException if the local host name could not be resolved into an address
-     *         ParseException if param are not fully set.
      */
-    public static void main(String[] args) throws UnknownHostException, ParseException {
-
+    public static void main(String[] args) throws UnknownHostException {
         SpringApplication app = new SpringApplication(EsmeurexengineApp.class);
-        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
-        addDefaultProfile(app, source);
+        addDefaultProfile(app);
         Environment env = app.run(args).getEnvironment();
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application '{}' is running! Access URLs:\n\t" +
@@ -102,13 +97,16 @@ public class EsmeurexengineApp {
     }
 
     /**
-     * If no profile has been configured, set by default the "dev" profile.
+     * set a default to use when no profile is configured.
      */
-    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
-        if (!source.containsProperty("spring.profiles.active") &&
-                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
-
-            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
-        }
+    protected static void addDefaultProfile(SpringApplication app) {
+        Map<String, Object> defProperties =  new HashMap<>();
+        /*
+        * The default profile to use when no other profiles are defined
+        * This cannot be set in the `application.yml` file.
+        * See https://github.com/spring-projects/spring-boot/issues/1219
+        */
+        defProperties.put("spring.profiles.default", Constants.SPRING_PROFILE_DEVELOPMENT);
+        app.setDefaultProperties(defProperties);
     }
 }
